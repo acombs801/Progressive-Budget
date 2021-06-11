6fca38eb962760f9951b878cde3684555a2a9ea6
Adrian <74800016+acombs801@users.noreply.github.com>
2021-06-11 00:47:51 -0700

    testing

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
   "/icons/icon-512x512.png"
 ];
 
-const CACHE_NAME = "my-site-cache-v1";
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function(evt) {
